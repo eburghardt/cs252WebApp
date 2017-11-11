0c60dfff9d0e437d5c403b64ac4d86e8f217f69c
Eric Michael Burghardt <eburghar@data.cs.purdue.edu>
2017-11-11 01:34:53 -0500

    Finished dictionary implementation

--- src/dictionary.cpp
+++ src/dictionary.cpp
@@ -7,6 +7,11 @@
  * @date 11/11/2017
  */
 
+#include <string>	//string manipulations
+#include <fstream>	//reading dictionary file
+#include <iostream>	//I/O
+#include <locale>	// locale, tolower
+
 using namespace std;
 
 const int ALPHABET_SIZE = 26; //26 characters in the english alphabet
@@ -29,4 +34,86 @@ Node * createNode() { //Constructor method
 	return newNode;
 }
 
+//Add a word to the trie if not present
+void add(Node * root, string word) {
+	Node * current = root;
+	locale loc;
+	for(int i = 0; i < word.length(); i++) {
+		int currChar = tolower(word[i], loc) - 'a'; //Convert current character to index
+		if(currChar < 0 || currChar >= ALPHABET_SIZE) {
+			cout << "Something happened during insert\n";
+			return;
+		}
+		
+		if(!current->children[currChar]) //Need to create a new node
+			current->children[currChar] = createNode();
+		
+		current = current->children[currChar]; //Set current node to index of current character
+	}
+	//Mark last node as a word. May not be a leaf because may be a prefix.
+	current->isWordEnd = true;
+}
 
+//Check if a word exists in the trie
+//Returns true if word exists
+//Returns false if word does not exist
+bool contains(Node * root, string word) {
+	Node * current = root;
+	locale loc;
+	for(int i = 0; i < word.length(); i++) {
+		int currChar = tolower(word[i], loc) - 'a'; //Convert current character to index
+		if(currChar < 0 || currChar >= ALPHABET_SIZE) {
+			cout << "Something happened during search\n";
+			return false;
+		}
+
+		if(!current->children[currChar]) //If node is not present
+			return false;
+
+		//node is present, move to next character
+		current = current->children[currChar];
+	}
+	return (current != NULL && current->isWordEnd);
+}
+
+
+
+
+
+class Dictionary {
+	private:
+		Node * root;
+		string fileName;	
+	public:
+		Dictionary(string fn) {
+			fileName = fn;
+			root = createNode();
+
+			//Build trie
+			
+			//First, open file, then, line by line, read the words into the trie
+			ifstream infile(fn.c_str());
+			string line;
+			while(getline(infile, line)) {
+				add(root, line);
+			}
+			
+		}
+
+		bool isWord(string word) {
+			return contains(root, word);
+		}		
+};
+
+
+int main(int argc, char ** argv) {
+	//For testing purposes
+	Dictionary dictionary("../assets/dictionary.txt");
+
+	for(int i = 0; i < argc; i++) {
+		if(i == 0)
+			continue;
+		cout << "Is word: " << argv[i] << "?\n";
+		cout << dictionary.isWord(argv[i]) << "\n";
+	}
+}
